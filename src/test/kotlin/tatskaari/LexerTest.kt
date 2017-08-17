e5a2c7d0f5c6d78f9b7a7c9d7b4ebf5e0e99932c
Jonathan <jonfpoole@gmail.com>
2017-08-17 21:36:03 +0100

    Reinstated lexer tests

--- src/test/kotlin/tatskaari/LexerTest.kt
+++ src/test/kotlin/tatskaari/LexerTest.kt
@@ -1,3 +1,4 @@
+
 package tatskaari
 
 import org.testng.annotations.Test
@@ -8,95 +9,98 @@ import kotlin.test.assertFailsWith
 object LexerTest {
 
 
-//  @Test
-//  fun testLexer() {
-//    val program = TestUtil.loadProgram("BasicBlocks")
-//    val tokenList = Lexer.lex(program)
-//    val expectedTokens = listOf(
-//      Token.Keyword(TokenType.OpenBlock),
-//      KeyWords.Val,
-//      Token.Identifier("someVariable"),
-//      KeyWords.AssignOp,
-//      Token.Num(12),
-//      KeyWords.CloseBlock
-//    )
-//
-//    assert(tokenList.size == expectedTokens.size)
-//    tokenList.zip(expectedTokens)
-//      .forEach { (actual, expect) -> assertEquals(expect, actual) }
-//  }
+  @Test
+  fun testLexer() {
+    val program = TestUtil.loadProgram("BasicBlocks")
+    val tokenList = Lexer.lex(program)
+    val expectedTokens = listOf(
+      TokenType.OpenBlock,
+      TokenType.Val,
+      TokenType.Identifier,
+      TokenType.AssignOp,
+      TokenType.Num,
+      TokenType.CloseBlock
+    )
+
+    assert(tokenList.size == expectedTokens.size)
+    tokenList.zip(expectedTokens)
+      .forEach { (actual, expect) -> assertEquals(expect, actual.tokenType) }
+  }
 
   @Test
   fun testInvalidToken() {
     assertFailsWith<Lexer.InvalidInputException> { Lexer.lex("{ var a := 123 } []';") }
   }
 
-//  @Test
-//  fun testAssignExpression() {
-//    val program = "{val someVariable := + 12 12}"
-//    val tokenList = Lexer.lex(program)
-//    val expectedTokens = listOf(
-//      KeyWords.OpenBlock,
-//      KeyWords.Val,
-//      Token.Identifier("someVariable"),
-//      KeyWords.AssignOp,
-//      KeyWords.Add,
-//      Token.Num(12),
-//      Token.Num(12),
-//      KeyWords.CloseBlock
-//    )
-//
-//    assert(tokenList.size == expectedTokens.size)
-//    tokenList.zip(expectedTokens)
-//      .forEach { (actual, expect) -> assertEquals(actual, expect) }
-//  }
+  @Test
+  fun testAssignExpression() {
+    val program = "{val someVariable := + 12 12}"
+    val tokenList = Lexer.lex(program)
+    val expectedTokens = listOf(
+      TokenType.OpenBlock,
+      TokenType.Val,
+      TokenType.Identifier,
+      TokenType.AssignOp,
+      TokenType.Add,
+      TokenType.Num,
+      TokenType.Num,
+      TokenType.CloseBlock
+    )
 
-//  @Test
-//  fun testIfStatement() {
-//    val program = "{if (1 = 1) {val someVariable := 2}}"
-//    val tokenList = Lexer.lex(program)
-//    val expectedTokens = listOf(
-//      KeyWords.OpenBlock,
-//      KeyWords.If,
-//      KeyWords.OpenParen,
-//      Token.Num(1),
-//      KeyWords.Equality,
-//      Token.Num(1),
-//      KeyWords.CloseParen,
-//      KeyWords.OpenBlock,
-//      KeyWords.Val,
-//      Token.Identifier("someVariable"),
-//      KeyWords.AssignOp,
-//      Token.Num(2),
-//      KeyWords.CloseBlock,
-//      KeyWords.CloseBlock
-//    )
-//    assert(tokenList.size == expectedTokens.size)
-//    tokenList.zip(expectedTokens)
-//      .forEach { (actual, expect) -> assertEquals(actual, expect) }
-//  }
-//
-//  @Test
-//  fun testInputOutput() {
-//    val program = "input output"
-//    val tokens = Lexer.lex(program)
-//    val expectedTokens = listOf<Token>(KeyWords.Input, KeyWords.Output)
-//    assertEquals(tokens, expectedTokens)
-//  }
-//
-//  @Test
-//  fun notTest() {
-//    val program = "!true"
-//    val tokens = Lexer.lex(program)
-//    val expectedTokens = listOf<Token>(KeyWords.Not, KeyWords.True)
-//    assertEquals(tokens, expectedTokens)
-//  }
-//
-//  @Test
-//  fun notEqTest() {
-//    val program = Lexer.lex("1 != 2")
-//    val expectedTokens = listOf<Token>(Token.Num(1), KeyWords.NotEquality, Token.Num(2))
-//    assertEquals(expectedTokens, program)
-//
-//  }
+    assert(tokenList.size == expectedTokens.size)
+    tokenList.zip(expectedTokens)
+      .forEach { (actual, expect) -> assertEquals(expect, actual.tokenType) }
+  }
+
+  @Test
+  fun testIfStatement() {
+    val program = "{if (1 = 1) {val someVariable := 2}}"
+    val tokenList = Lexer.lex(program)
+    val expectedTokens = listOf(
+      TokenType.OpenBlock,
+      TokenType.If,
+      TokenType.OpenParen,
+      TokenType.Num,
+      TokenType.Equality,
+      TokenType.Num,
+      TokenType.CloseParen,
+      TokenType.OpenBlock,
+      TokenType.Val,
+      TokenType.Identifier,
+      TokenType.AssignOp,
+      TokenType.Num,
+      TokenType.CloseBlock,
+      TokenType.CloseBlock
+    )
+    assert(tokenList.size == expectedTokens.size)
+    tokenList.zip(expectedTokens)
+      .forEach { (actual, expect) -> assertEquals(actual.tokenType, expect) }
+  }
+
+  @Test
+  fun testInputOutput() {
+    val program = "input output"
+    val tokens = Lexer.lex(program)
+    val expectedTokens = listOf(TokenType.Input, TokenType.Output)
+    tokens.zip(expectedTokens)
+      .forEach { (actual, expect) -> assertEquals(actual.tokenType, expect) }
+  }
+
+  @Test
+  fun notTest() {
+    val program = "!true"
+    val tokens = Lexer.lex(program)
+    val expectedTokens = listOf(TokenType.Not, TokenType.True)
+    tokens.zip(expectedTokens)
+      .forEach { (actual, expect) -> assertEquals(actual.tokenType, expect) }
+  }
+
+  @Test
+  fun notEqTest() {
+    val program = Lexer.lex("1 != 2")
+    val expectedTokens = listOf(TokenType.Num, TokenType.NotEquality, TokenType.Num)
+    program.zip(expectedTokens)
+      .forEach { (actual, expect) -> assertEquals(actual.tokenType, expect) }
+
+  }
 }
