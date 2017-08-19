fc6ffcd82d61028e5edd7ed61e70f735136b55b9
Jonathan <jonfpoole@gmail.com>
2017-08-19 23:29:15 +0100

    added string literals:wq

--- src/main/kotlin/tatskaari/tokenising/Token.kt
+++ src/main/kotlin/tatskaari/tokenising/Token.kt
@@ -3,7 +3,10 @@ sealed class Token (val tokenType: TokenType, val tokenText: String, val lineNum
 
   class Keyword(type: TokenType, text: String, line: Int, col: Int): Token(type, text, line, col)
   class Identifier(type: TokenType, val name: String, line: Int, col: Int): Token(TokenType.Identifier, name, line, col)
+  class TextLiteral(type: TokenType, text: String, line: Int, col: Int) : Token(TokenType.TextLiteral, text, line, col) {
+    val text: String = text.substring(1, text.length - 1).replace("\"\"", "\"")
 
+  }
   class Num(type: TokenType, value: String, line: Int, col: Int) : Token(type, value, line, col) {
     val value: Int = value.toInt()
   }
