99b8d180949f02a4d4ade92a6f340336fc7cfabb
Jon Poole <jonfpoole@gmail.com>
2017-11-05 13:23:54 +0000

    Reinstated strange missing test

--- src/test/kotlin/tatskaari/MainTest.kt
+++ src/test/kotlin/tatskaari/MainTest.kt
@@ -1,2 +1,10 @@
 package tatskaari
 
+import org.testng.annotations.Test
+
+object MainTest {
+  @Test
+  fun testDoesntError() {
+    Main.main(listOf(TestUtil.getProgramPath("TestMain")).toTypedArray())
+  }
+}
